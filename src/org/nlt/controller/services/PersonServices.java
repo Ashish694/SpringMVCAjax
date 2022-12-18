937d4e05bbf34ab8990d61e7a19d5214895869ee
Ashish Pardhi1 <chetanchitriv0@gmail.com>
2022-12-18 10:49:24 +0530

    adding springmvcajax

---
+++ src/org/nlt/controller/services/PersonServices.java
@@ -0,0 +1,71 @@
+package org.nlt.controller.services;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.classic.Session;
+import org.nlt.listener.DatabaseConnection;
+import org.nlt.model.Persons;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonServices
+{
+
+	public boolean submitPersonService(String name, int age, Long phone, String city, String state)
+	{
+		Persons person=new Persons();
+		person.setName(name);
+		person.setAge(age);
+		person.setPhone(phone);
+	//	person.setCity(city);
+	//	person.setState(state);
+		person.setStatus(1);
+		
+		Session ses=DatabaseConnection.getDatabaseSession();
+		ses.beginTransaction();
+		ses.save(person);
+		ses.getTransaction().commit();
+		return true;
+	}
+	
+	public List<Persons> getPersonList()
+	{
+		Session ses=DatabaseConnection.getDatabaseSession();
+		ses.beginTransaction();
+		Query query = ses.createQuery("from Persons where status=1");
+		List list=query.list();
+		ses.getTransaction().commit();
+		return list;
+	}
+	
+	public Persons getPersonRecord(int id) 
+	{
+		Session ses=DatabaseConnection.getDatabaseSession();
+		ses.beginTransaction();
+		Persons person = (Persons) ses.get(Persons.class, id);
+		ses.beginTransaction().commit();
+		return person;
+	}
+
+	public boolean updatePersonService(Persons person)
+	{
+		Session ses=DatabaseConnection.getDatabaseSession();
+		ses.beginTransaction();
+		ses.update(person);
+		ses.getTransaction().commit();
+		return true;
+	}
+
+	
+
+	public boolean deletePersonService(Persons person)
+	{
+		Session ses=DatabaseConnection.getDatabaseSession();
+		ses.beginTransaction();
+		ses.update(person);
+		ses.getTransaction().commit();
+		return true;
+	}
+
+}
